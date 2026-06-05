3aa4c42f460b909020c6f3a2a089d392a1f29565
Razi <dorra.razi@gmail.com>
2026-06-05 12:59:04 +0200

    changes added

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,6 @@ function App() {
   return (
     <div className="min-h-screen bg-base-200">
       <Header total={0} done={0} />
-      <main className="max-w-5xl mx-auto p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
-        <div className="md:col-span-1">{/* <TaskForm /> */}</div>
-
-        {}
-        <div className="md:col-span-2 flex flex-col gap-4">
-          {}
-          {}
-        </div>
-      </main>
     </div>
   );
 }
